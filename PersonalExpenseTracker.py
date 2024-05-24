c786f78c84daf058bd108d3b1f7e187568d27fbf
Charith <114335319+Charith0401@users.noreply.github.com>
2024-05-24 21:01:08 +0530

    input validations changed + partially completed summary of expenses module

--- PersonalExpenseTracker.py
+++ PersonalExpenseTracker.py
@@ -1,13 +1,13 @@
-import tkinter as tk
 import sqlite3
 import re #regex in python
 from tabulate import tabulate
 from datetime import datetime
+import calendar
 
 conn=sqlite3.connect('pet.db') #database used for project
 cursor=conn.cursor() #allows queries to be made
 
-def handleValidation(field): #function that handles validation for the all the fields in the expenses table (field corresponds to position in table)
+def handleUserInput(field): #function that handles validation for the all the fields in the expenses table (field corresponds to position in table)
     if (field==0):
         prompt=str(input("\nPlease Enter The Title: "))
     elif (field==1):
@@ -17,12 +17,12 @@ def handleValidation(field): #function that handles validation for the all the f
     else:
         prompt=int(input("\nPlease Enter The Expense Amount: "))
     
-    if (field!=2):
+    if (field==0 or field==1): # if the field is expense name or category
         while (prompt=="" and re.search(r'\d',prompt)):
-                prompt=str(input("\nPlease Enter An Appropriate Title: "))
+                prompt=str(input("\nPlease Enter Valid Data (characters only): "))
                 if (len(prompt)>0 and prompt.isdigit()==False):
                     break
-    else:
+    elif (field==2): # if the field is a date
         valid=False
         while (valid==False):
                 try:
@@ -31,15 +31,19 @@ def handleValidation(field): #function that handles validation for the all the f
                 except:
                     prompt=str(input("\nPlease Enter An Appropriate Date: "))
                     valid=False
+    else: # if the field is amount
+         while (prompt=="" and re.search(r'\d',prompt)==False):
+                prompt=str(input("\nPlease Enter Valid Data (digits only): "))
+                if (len(prompt)>0 and prompt.isdigit()==False):
+                    break
     return prompt
 
 def handleAddExpense():
-    # label.config(text="Expense Added")
-    print("========Adding An Expense========")
-    title=handleValidation(0)
-    category=handleValidation(1)
-    date=handleValidation(2)
-    expense=handleValidation(3)
+    print("ADDING AN EXPENSE")
+    title=handleUserInput(0)
+    category=handleUserInput(1)
+    date=handleUserInput(2)
+    expense=handleUserInput(3)
 
     try:
         cursor.execute("INSERT INTO expenses (title,category,date,amount) VALUES (?,?,?,?)",(title,category,date,expense))
@@ -48,51 +52,60 @@ def handleAddExpense():
     except:
         print("ERROR\n")
     
-def handleViewExpenses():
+def retrieveCurrentExpenses():
+    print("CURRENT EXPENSES")
     cursor.execute("SELECT * FROM expenses")
     records=cursor.fetchall()
-    print(tabulate(records,headers=["ID","Title","Category","Date","Amount"]))
-    # for i in range(0,len(records)):
-    #     print(tabulate(records[i]))
+    return records
 
 def handleDeleteExpense():
-    pass
+    print("DELETING AN EXPENSE")
+    retrieveCurrentExpenses()
+    id=str(input("Please Enter The ID Of The Expense To Be Deleted: "))
+    try:
+        cursor.execute("DELETE FROM expenses WHERE id=?",(id))
+        conn.commit()
+    except:
+        print("ERROR: ID Does Not Exist")
 
 def handleSummaryOfExpenses():
-    pass
-
+    records=retrieveCurrentExpenses()
+    inputMonth=int(input("Please Enter A Month Number To Filter: "))
+    dummyArray=[]
+    if (inputMonth>0 and inputMonth<=12):
+        print("EXPENSES FOR MONTH OF ",calendar.month_name[inputMonth])
+        for j in range (0,len(records)):
+            temp=datetime.strptime(records[j][3],'%d-%m-%y')
+            if (temp.month==inputMonth):
+                dummyArray.append(records[j])
+                # print(tabulate(records[j],headers=["ID","Title","Category","Date","Amount"]))
+                # print(tabulate(records,headers=["ID","Title","Category","Date","Amount"]))
+            print(tabulate(dummyArray,headers=["ID","Title","Category","Date","Amount"]))
+    else:
+        
 
 def handleUserSelection(option):
     if (option==1):
         handleAddExpense()
     elif (option==2):
-        handleViewExpenses()
+        records=retrieveCurrentExpenses()
+        print(tabulate(records,headers=["ID","Title","Category","Date","Amount"]))
     elif (option==3):
         handleDeleteExpense()
     else:
         handleSummaryOfExpenses()
     
 def onLoad():
-    # root.title("Personal Expense Tracker")
-    # root.geometry("1200x800")
-
-    # label=tk.Label(root,text="Current Expenses")
-    # label.pack()
-
-    # button=tk.Button(root,text="Add An Expense",command=handleAddExpense)
-    # button.pack()
     print("\nOPTIONS AVAILABLE:\n1.Add Expense\n2.View Expenses\n3.Delete Expense\n4.Summary Of Expenses Per Month\n")
     try:
         option=int(input("Please Choose An Option: "))
         handleUserSelection(option)
     except:
         onLoad()
-    # return label
 
-# root=tk.Tk()
 stop=0
 while (stop==0):
     onLoad()
-# root.mainloop()
+
     
     
--- start.bat
+++ start.bat
@@ -1,5 +1,3 @@
-python -m venv venv
 call venv\Scripts\activate
-pip install -r requirements.txt
 python PersonalExpenseTracker.py
 pause
